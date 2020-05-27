db1eb333f385cb2f15099494735538d34061b066
stephenJohnGiardina <stephengia@yahoo.com>
2020-05-27 14:21:52 -0400

    Updated PostgreSQL/database.js

--- database/PostgreSQL/database.js
+++ database/PostgreSQL/database.js
@@ -1,18 +1,56 @@
-const { Sequelize } = require('sequelize');
+const { Pool } = require('pg');
+const pgtools = require('pgtools');
+const PASSWORD = require('../passwordIgnore.js');
 
-const sequelize = new Sequelize('images', 'postgres', 'Wantagh1!@#', {
+const config = {
+  user: 'postgres',
   host: 'localhost',
-  dialect: 'postgres'
-});
-
-let x = async () => {
-
-  try {
-    await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
+  password: PASSWORD.PASSWORD,
+  port: 5432,
 }
 
-x()
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'images',
+  password: PASSWORD.PASSWORD,
+  port: 5432
+});
+
+setTimeout(() => {
+    pgtools.dropdb(config, 'images', (err, res) => {
+      if (err) {
+        console.log('Nothing to drop.');
+      } else {
+        console.log('Database dropped.');
+      }
+    });
+  }, 1000
+);
+
+setTimeout( async () => {
+    await pgtools.createdb(config, 'images', (err, response) => {
+      if (err) {
+        console.log(err);
+      } else {
+        pool.connect()
+        .then(() => console.log('Connected to pool.'));
+        console.log('Created database!');
+        createTable()
+      }
+    });
+  }, 2000
+  );
+
+  const createTable = () => {
+    pool.query('CREATE TABLE images (id serial, image varchar(200))',  (err, res) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('created table');
+      }
+      process.exit();
+  });
+}
+
+module.exports = pool;
