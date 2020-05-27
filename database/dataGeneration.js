643dbb26caccf58b75cf7a5e6e79e734ec180295
stephenJohnGiardina <stephengia@yahoo.com>
2020-05-27 14:18:49 -0400

    Updated dataGeneration

--- database/dataGeneration.js
+++ database/dataGeneration.js
@@ -1,11 +1,32 @@
-const generateData = () => {
-  let data = [];
-  for (let i = 0; i < 357143; i++) {
-    for (let j = 1; j < 29; j++) {
-      data.push(`https://s3.amazonaws.com/carousel-images.bucket/${j}-370x370.jpg`);
+const fs = require('fs');
+
+const writeUrl = fs.createWriteStream('data.csv', {flags: 'w'});
+writeUrl.write('image\n', 'utf8');
+
+function writeTenMillionUrl(writer, encoding, callback) {
+  let i = 10000000;
+  let id = 0;
+  function write() {
+    let ok = true;
+    do {
+      i -= 1;
+      id += 1;
+      const url = `https://s3.amazonaws.com/carousel-images.bucket/${id % 28 + 1}-370x370.jpg`;
+      const data = `${url}\n`;
+      if (i === 0) {
+        writer.write(data, encoding, callback);
+      } else {
+        ok = writer.write(data, encoding);
+      }
+    } while (i > 0 && ok);
+    if (i > 0) {
+      writer.once('drain', write);
     }
   }
-  return data;
+  write()
 }
-console.log(typeof generateData());
-module.exports.generateData = generateData;
+
+writeTenMillionUrl(writeUrl, 'utf-8', () => {
+  console.log('Finished generating data!')
+  writeUrl.end();
+});
